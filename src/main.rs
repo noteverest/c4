65162d425e9e17753b28708108d9b0fd192a9d4d
Greg Taylor <noteverest@gmail.com>
2020-08-08 17:12:23 -0400

    Make Connect 4 playable
    No win detection

--- src/main.rs
+++ src/main.rs
@@ -2,58 +2,137 @@ extern crate rand;
 
 // rust has something like c++ namespaces
 use std::fmt;
+use std::cmp;
+//use std::ops;
 
 // global board size constants
 const BOARD_ROWS: usize = 6;
 const BOARD_COLS: usize = 7;
+const WIN_CNT: usize = 4;
 
-#[derive(Copy)]
+#[derive(Copy,Clone,Eq,PartialEq)]
 enum BoardSpace {
     Empty,
-    Player1,
-    Player2,
+    Player(u8),
 }
 
 impl fmt::Display for BoardSpace {
     fn fmt( &self, f: &mut fmt::Formatter ) -> fmt::Result {
         match self {
             BoardSpace::Empty => write!( f, " " ),
-            BoardSpace::Player1 => write!( f, "O" ),
-            BoardSpace::Player2 => write!( f, "X" ),
+            BoardSpace::Player(1) => write!( f, "O" ),
+            BoardSpace::Player(2) => write!( f, "X" ),
+            BoardSpace::Player(i) => write!( f, "{}", i ),
         }
     }
 }
 
 fn main() {
-    println!( "Guess the number!" );
+    println!( "Connect {}", WIN_CNT );
 
-    let mut row: [BoardSpace; BOARD_COLS] = [ BoardSpace::Empty; BOARD_COLS ];
-    let mut board: [[BoardSpace; BOARD_COLS]; BOARD_ROWS] = [ row; BOARD_ROWS ];
+    //let mut row: [BoardSpace; BOARD_COLS] = [ BoardSpace::Empty; BOARD_COLS ];
+    //let mut board: [[BoardSpace; BOARD_COLS]; BOARD_ROWS] = [ row; BOARD_ROWS ];
+    //print_board( board );
 
-    print_board( board );
+    let mut board: [BoardSpace; BOARD_ROWS * BOARD_COLS ] = [BoardSpace::Empty; BOARD_ROWS * BOARD_COLS];
+    let mut winner: BoardSpace = BoardSpace::Empty;
+    let mut turn: u32 = 1;
 
-    /*
     loop {
-        println!( "Please input your guess." );
+        println!( "Turn {}", turn );
+        print_board( board );
 
-        let mut guess = String::new();
+        let mut input = String::new();
 
-        std::io::stdin().read_line( &mut guess )
-            .expect( "Failed to read line" );
+        std::io::stdin().read_line( &mut input )
+            .expect( "failed to get" );
 
-        let guess : u32 = match guess.trim().parse() {
+        let input : usize = match input.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!( "You guessed: {}", guess );
+        if turn % 2 == 0 {
+            if !place_thing( &mut board, BoardSpace::Player(1), input ) {
+                println!( "didn't work, guess you lost your turn, dummy" );
+            }
+        } else {
+            if !place_thing( &mut board, BoardSpace::Player(2), input ) {
+                println!( "didn't work, guess you lost your turn, dummy" );
+            }
+        }
+        println!( "Turn {}", turn );
+        print_board( board );
 
-        match guess.cmp( &secret_number ) {
-            std::cmp::Ordering::Less => println!( "Too small!" ),
-            std::cmp::Ordering::Greater => println!( "Too big!" ),
-            std::cmp::Ordering::Equal => {
-                println!( "You win!" );
-                break;
+        winner = check_victory( board );
+        if winner != BoardSpace::Empty {
+            println!( "A WINNER IS {}", winner );
+            break;
+        }
+
+        turn += 1;
+    }
+}
+
+fn print_board( board: [BoardSpace; BOARD_ROWS * BOARD_COLS ] ) {
+    // ledger
+    for i in 0..BOARD_COLS {
+        print!( " {}", i );
+    }
+    println!( "" );
+    for i in 0..(BOARD_ROWS * BOARD_COLS) {
+        print!( "|{}", board[i] );
+        if i % BOARD_COLS == BOARD_COLS - 1 {
+            println!( "|" );
+        }
+    }
+}
+
+fn place_thing( board: &mut [BoardSpace; BOARD_ROWS * BOARD_COLS ], player: BoardSpace, col: usize ) -> bool {
+    let mut found : bool = false;
+    let mut found_row : usize = 0;
+
+    // iterate down column
+    for row in 0..BOARD_ROWS {
+        // remember empty spaces
+        if board[row * BOARD_COLS + col] == BoardSpace::Empty {
+            found = true;
+            found_row = row;
+        }
+    }
+
+    // place in last found empty space
+    if found == true {
+        board[found_row * BOARD_COLS + col] = player;
+    }
+
+    return found;
+}
+
+fn check_victory( board: [BoardSpace; BOARD_ROWS * BOARD_COLS ] ) -> BoardSpace {
+    let mut player: BoardSpace = BoardSpace::Empty;
+    let mut count : usize = 0;
+
+    // check rows
+    for row in 0..BOARD_ROWS {
+        for col in 0..BOARD_COLS {
+            match board[BOARD_COLS * row + col] {
+                BoardSpace::Empty => {
+                    player = BoardSpace::Empty;
+                    count = 0;
+                },
+                BoardSpace::Player(p) => {
+                    if player == BoardSpace::Player(p) {
+                        count += 1;
+                    } else {
+                        player = BoardSpace::Player(p);
+                        count = 1;
+                    }
+                },
+            }
+            println!( "row - check ({}, {}) - cnt {} {}", row, col, count, player );
+            if count == WIN_CNT {
+                return player;
             }
         }
     }
