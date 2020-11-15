75bca07ee52399b240cabcbe573cad3c62672157
Greg Taylor <noteverest@gmail.com>
2020-11-15 16:04:20 -0500

    Playable Connect 4 with win detection
    Made the player count fully adjustable, sacrificing X/O symbols for
    numbers in the process. Player count also upgraded to u32.
    
    Replaced the convoluted and broken diagonals checking with a new method
    that is simpler. Details documented within.
    
    Lots of faffing about with types. Had been using usize universally,
    but now most values are u32 until needing to be used as an array index.
    The new diagonals checking has to do some unsigned/signed type
    conversions and it isn't pretty but it seems to work.
    
    Also improved debug output for win detection, removed some vestigial
    stuff, and added more comments and TODOs.

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,26 @@ extern crate rand;
 // rust has something like c++ namespaces
 // TODO: module system weird
 use std::fmt;
-use std::cmp;
+//use std::cmp;
 //use std::ops;
 
 // global board size constants
-const BOARD_ROWS: usize = 6;
-const BOARD_COLS: usize = 7;
-const WIN_CNT: usize = 4;
+const BOARD_ROWS: u32 = 6;
+const BOARD_COLS: u32 = 7;
+const BOARD_SIZE: usize = ( BOARD_ROWS * BOARD_COLS ) as usize;
+const PLAYERS: u32 = 2;
+const WIN_CNT: u32 = 4;
 
 #[derive(Copy,Clone,Eq,PartialEq)]
 enum BoardSpace {
     Empty,
-    Player(u8),
+    Player(u32),
 }
 
 impl fmt::Display for BoardSpace {
     fn fmt( &self, f: &mut fmt::Formatter ) -> fmt::Result {
         match self {
             BoardSpace::Empty => write!( f, " " ),
-            BoardSpace::Player(1) => write!( f, "O" ),
-            BoardSpace::Player(2) => write!( f, "X" ),
             BoardSpace::Player(i) => write!( f, "{}", i ),
         }
     }
@@ -31,72 +31,72 @@ impl fmt::Display for BoardSpace {
 fn main() {
     println!( "Connect {}", WIN_CNT );
 
-    //let mut row: [BoardSpace; BOARD_COLS] = [ BoardSpace::Empty; BOARD_COLS ];
-    //let mut board: [[BoardSpace; BOARD_COLS]; BOARD_ROWS] = [ row; BOARD_ROWS ];
-    //print_board( board );
-
-    let mut board: [BoardSpace; BOARD_ROWS * BOARD_COLS ] = [BoardSpace::Empty; BOARD_ROWS * BOARD_COLS];
-    let mut winner: BoardSpace = BoardSpace::Empty;
-    let mut turn: u32 = 1;
+    let mut board: [BoardSpace; BOARD_SIZE] = [BoardSpace::Empty; BOARD_SIZE];
+    let mut turn: u32 = 0;
 
     loop {
-        println!( "Turn {}", turn );
+        // Print board state
+        let player = turn % PLAYERS;
+        println!( "Turn {} - Player {}", turn + 1, player );
         print_board( board );
 
-        let mut input = String::new();
-
-        std::io::stdin().read_line( &mut input )
-            .expect( "failed to get" );
-
-        let input : usize = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        if turn % 2 == 0 {
-            if !place_thing( &mut board, BoardSpace::Player(1), input ) {
-                println!( "didn't work, guess you lost your turn, dummy" );
-            }
-        } else {
-            if !place_thing( &mut board, BoardSpace::Player(2), input ) {
-                println!( "didn't work, guess you lost your turn, dummy" );
+        // Make move
+        let mut played = false;
+        while !played  {
+            let mut input = String::new();
+            std::io::stdin().read_line( &mut input )
+                .expect( "failed to get" );
+            let input : u32 = match input.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            // TODO: check for moves that are out of bounds!!!
+            played = make_move( &mut board, BoardSpace::Player(player), input );
+            if !played {
+                println!( "Invalid move -- try again" );
             }
         }
-        println!( "Turn {}", turn );
+
+        // Print board state again?
+        println!( "Turn {} - Player {}", turn + 1, player );
         print_board( board );
 
-        winner = check_victory( board );
+        // Check for winner
+        let winner = check_victory( board );
         if winner != BoardSpace::Empty {
-            println!( "A WINNER IS {}", winner );
+            println!( "PLAYER {} IS THE WINNER", winner );
             break;
         }
 
+        // Next turn
         turn += 1;
     }
 }
 
-fn print_board( board: [BoardSpace; BOARD_ROWS * BOARD_COLS ] ) {
-    // ledger
+fn print_board( board: [BoardSpace; BOARD_SIZE] ) {
+    // column ledger
     for i in 0..BOARD_COLS {
         print!( " {}", i );
     }
     println!( "" );
-    for i in 0..(BOARD_ROWS * BOARD_COLS) {
+    for i in 0..BOARD_SIZE {
         print!( "|{}", board[i] );
-        if i % BOARD_COLS == BOARD_COLS - 1 {
+        if (i as u32) % BOARD_COLS == BOARD_COLS - 1 {
             println!( "|" );
         }
     }
 }
 
-fn place_thing( board: &mut [BoardSpace; BOARD_ROWS * BOARD_COLS ], player: BoardSpace, col: usize ) -> bool {
+fn make_move( board: &mut [BoardSpace; BOARD_SIZE], player: BoardSpace, col: u32 ) -> bool {
     let mut found : bool = false;
-    let mut found_row : usize = 0;
+    let mut found_row : u32 = 0;
+
+    // TODO: try iterating up column instead
 
     // iterate down column
     for row in 0..BOARD_ROWS {
         // remember empty spaces
-        if board[row * BOARD_COLS + col] == BoardSpace::Empty {
+        if board[(row * BOARD_COLS + col) as usize] == BoardSpace::Empty {
             found = true;
             found_row = row;
         }
@@ -104,20 +104,21 @@ fn place_thing( board: &mut [BoardSpace; BOARD_ROWS * BOARD_COLS ], player: Boar
 
     // place in last found empty space
     if found == true {
-        board[found_row * BOARD_COLS + col] = player;
+        board[(found_row * BOARD_COLS + col) as usize] = player;
     }
 
     return found;
 }
 
-fn check_victory( board: [BoardSpace; BOARD_ROWS * BOARD_COLS ] ) -> BoardSpace {
+fn check_victory( board: [BoardSpace; BOARD_SIZE] ) -> BoardSpace {
     let mut player: BoardSpace = BoardSpace::Empty;
-    let mut count : usize = 0;
+    let mut count : u32 = 0;
 
     // check rows
     for row in 0..BOARD_ROWS {
         for col in 0..BOARD_COLS {
-            match board[BOARD_COLS * row + col] {
+            let idx = (BOARD_COLS * row + col) as usize;
+            match board[idx] {
                 BoardSpace::Empty => {
                     player = BoardSpace::Empty;
                     count = 0;
@@ -131,7 +132,7 @@ fn check_victory( board: [BoardSpace; BOARD_ROWS * BOARD_COLS ] ) -> BoardSpace 
                     }
                 },
             }
-            println!( "row - check ({}, {}) - cnt {} {}", row, col, count, player );
+            //println!( "rows - ({}, {}) - got '{}' - '{}' has {}", row, col, board[idx], player, count );
             if count == WIN_CNT {
                 return player;
             }
@@ -141,7 +142,8 @@ fn check_victory( board: [BoardSpace; BOARD_ROWS * BOARD_COLS ] ) -> BoardSpace 
     // check columns
     for col in 0..BOARD_COLS {
         for row in 0..BOARD_ROWS {
-            match board[BOARD_COLS * row + col] {
+            let idx = (BOARD_COLS * row + col) as usize;
+            match board[idx] {
                 BoardSpace::Empty => {
                     player = BoardSpace::Empty;
                     count = 0;
@@ -155,185 +157,83 @@ fn check_victory( board: [BoardSpace; BOARD_ROWS * BOARD_COLS ] ) -> BoardSpace 
                     }
                 },
             }
-            println!( "col - check ({}, {}) - cnt {} {}", row, col, count, player );
+            //println!( "cols - ({}, {}) - got '{}' - '{}' has {}", row, col, board[idx], player, count );
             if count == WIN_CNT {
                 return player;
             }
         }
     }
 
-    // build diagonal lengths
-    let len_diag: usize = cmp::min( BOARD_ROWS, BOARD_COLS );
-    let mut len_diag_rows: [usize; BOARD_ROWS - 1] = [0; BOARD_ROWS - 1];
-    let mut len_diag_cols: [usize; BOARD_COLS - 1] = [0; BOARD_COLS - 1];
+    /* Strategy for checking diagonals:
+        Always iterate left to right across the board
+        The board is 'extended' vertically, we 'check' many off-board spaces
+        We start checking with the 'highest' diagonal
+            diags1 run NW to SE
+                first diags1 diagonal starts 'over' the board
+                intersects only the top-right board space
+            diags2 run SW to NE
+                first diags2 diagonal starts in the top-left board space
+                the rest extends up 'over' the board
+        Each following diagonal check starts vertically one space lower,
+            until the whole board is checked
+    */
 
-    // fill in row diagonal lengths
-    for r in (1..BOARD_ROWS).rev() {
-        if BOARD_ROWS > BOARD_COLS {
-            let diff = BOARD_COLS - BOARD_ROWS;
-            if r < diff {
-                len_diag_rows[r] = BOARD_COLS;
+    // check diagonals 1 - Northwest / Southeast
+    for diag in 0..(BOARD_ROWS + BOARD_COLS - 1) {
+        for x in 0..BOARD_COLS {
+            let y : i32 = (BOARD_ROWS as i32) - (diag as i32) + (x as i32) - 1;
+            if y >= 0 && y < BOARD_ROWS as i32 {
+                let idx = (BOARD_COLS * y as u32 + x) as usize;
+                match board[idx] {
+                    BoardSpace::Empty => {
+                        player = BoardSpace::Empty;
+                        count = 0;
+                    },
+                    BoardSpace::Player(p) => {
+                        if player == BoardSpace::Player(p) {
+                            count += 1;
+                        } else {
+                            player = BoardSpace::Player(p);
+                            count = 1;
+                        }
+                    },
+                }
+                //println!( "diags1 d{} - ({}, {}) - got '{}' - '{}' has {}", diag, x, y, board[idx], player, count );
             } else {
-                len_diag_rows[r] = BOARD_COLS - (r - diff + 1);
+                player = BoardSpace::Empty;
+                count = 0;
             }
-        } else {
-            // BOARD_COLS > BOARD_ROWS {
-            len_diag_rows[r] = BOARD_COLS - (r + 1);
+            if count == WIN_CNT {
+                return player;
+            }
         }
     }
 
-    // fill in column diagonal lengths
-    for c in 0..BOARD_COLS - 1 {
-        if BOARD_COLS > BOARD_ROWS {
-            let diff = BOARD_COLS - BOARD_ROWS;
-            if c < diff {
-                len_diag_cols[c] = BOARD_ROWS;
+    // check diagonals 2 - Northeast / Southwest
+    for diag in 0..(BOARD_ROWS + BOARD_COLS - 1) {
+        for x in 0..BOARD_COLS {
+            let y : i32 = (diag as i32) - (x as i32);
+            if y >= 0 && y < BOARD_ROWS as i32 {
+                let idx = (BOARD_COLS * y as u32 + x) as usize;
+                match board[idx] {
+                    BoardSpace::Empty => {
+                        player = BoardSpace::Empty;
+                        count = 0;
+                    },
+                    BoardSpace::Player(p) => {
+                        if player == BoardSpace::Player(p) {
+                            count += 1;
+                        } else {
+                            player = BoardSpace::Player(p);
+                            count = 1;
+                        }
+                    },
+                }
+                //println!( "diags2 d{} - ({}, {}) - got '{}' - '{}' has '{}'", diag, x, y, board[idx], player, count );
             } else {
-                len_diag_cols[c] = BOARD_ROWS - (c - diff + 1);
+                player = BoardSpace::Empty;
+                count = 0;
             }
-        } else {
-            // BOARD_ROWS >= BOARD_COLS {
-            len_diag_cols[c] = BOARD_ROWS - (c + 1);
-        }
-    }
-
-    // check diagonals, top-left <-> bottom-right
-
-    // check corner diagonal
-    for space in 0..cmp::min( BOARD_ROWS, BOARD_COLS ) {
-        match board[BOARD_COLS * (space) + space] {
-                BoardSpace::Empty => {
-                    player = BoardSpace::Empty;
-                    count = 0;
-                },
-                BoardSpace::Player(p) => {
-                    if player == BoardSpace::Player(p) {
-                        count += 1;
-                    } else {
-                        player = BoardSpace::Player(p);
-                        count = 1;
-                    }
-                },
-            }
-            println!( "d,rd  - check ({}, {}) - cnt {} {}", space, space, count, player );
-            if count == WIN_CNT {
-                return player;
-            }
-    }
-
-    // check from left side, top -> bottom
-    for diag in 0..BOARD_ROWS - (WIN_CNT - 1) - 1 {
-        for space in 0..len_diag_rows[diag] {
-            match board[BOARD_COLS * (diag + space + 1) + space] {
-                BoardSpace::Empty => {
-                    player = BoardSpace::Empty;
-                    count = 0;
-                },
-                BoardSpace::Player(p) => {
-                    if player == BoardSpace::Player(p) {
-                        count += 1;
-                    } else {
-                        player = BoardSpace::Player(p);
-                        count = 1;
-                    }
-                },
-            }
-            println!( "d,rd  - check ({}, {}) - cnt {} {}", diag+space+1, space, count, player );
-            if count == WIN_CNT {
-                return player;
-            }
-        }
-    }
-    // check from top row, left -> right
-    for diag in 0..BOARD_COLS - (WIN_CNT - 1) {
-        for space in 0..len_diag_cols[diag] {
-            match board[BOARD_COLS * space + (diag + space + 1)] {
-                BoardSpace::Empty => {
-                    player = BoardSpace::Empty;
-                    count = 0;
-                },
-                BoardSpace::Player(p) => {
-                    if player == BoardSpace::Player(p) {
-                        count += 1;
-                    } else {
-                        player = BoardSpace::Player(p);
-                        count = 1;
-                    }
-                },
-            }
-            println!( "d,rd  - check ({}, {}) - cnt {} {}", space, diag+space+1, count, player );
-            if count == WIN_CNT {
-                return player;
-            }
-        }
-    }
-
-    // check diagonals, bottom-left <-> top-right
-
-    // check corner diagonal
-    for space in 0..cmp::min( BOARD_ROWS, BOARD_COLS ) {
-        match board[BOARD_COLS * (BOARD_ROWS - 1 - space) + space] {
-                BoardSpace::Empty => {
-                    player = BoardSpace::Empty;
-                    count = 0;
-                },
-                BoardSpace::Player(p) => {
-                    if player == BoardSpace::Player(p) {
-                        count += 1;
-                    } else {
-                        player = BoardSpace::Player(p);
-                        count = 1;
-                    }
-                },
-            }
-            println!( "d,ru  - check ({}, {}) - cnt {} {}", BOARD_ROWS - space - 1, space, count, player );
-            if count == WIN_CNT {
-                return player;
-            }
-    }
-
-    // check from left side, bottom -> top
-    for diag in 0..BOARD_ROWS - (WIN_CNT - 1) {
-        for space in 0..cmp::min( BOARD_ROWS, BOARD_COLS ) - diag {
-            match board[BOARD_COLS * (BOARD_ROWS - 1 - diag - space) + space] {
-                BoardSpace::Empty => {
-                    player = BoardSpace::Empty;
-                    count = 0;
-                },
-                BoardSpace::Player(p) => {
-                    if player == BoardSpace::Player(p) {
-                        count += 1;
-                    } else {
-                        player = BoardSpace::Player(p);
-                        count = 1;
-                    }
-                },
-            }
-            println!( "d,ru - check ({}, {}) - cnt {} {}", BOARD_ROWS-diag-space, space, count, player );
-            if count == WIN_CNT {
-                return player;
-            }
-        }
-    }
-
-    // check from bottom row, left -> right
-    for diag in 0..BOARD_COLS - (WIN_CNT - 1) {
-        for space in 0..cmp::min( BOARD_ROWS, BOARD_COLS ) - diag {
-            match board[BOARD_COLS * (BOARD_ROWS - 1 - space) + diag + space] {
-                BoardSpace::Empty => {
-                    player = BoardSpace::Empty;
-                    count = 0;
-                },
-                BoardSpace::Player(p) => {
-                    if player == BoardSpace::Player(p) {
-                        count += 1;
-                    } else {
-                        player = BoardSpace::Player(p);
-                        count = 1;
-                    }
-                },
-            }
-            println!( "d,ru  - check ({}, {}) - cnt {} {}", BOARD_ROWS-space, diag+space, count, player );
             if count == WIN_CNT {
                 return player;
             }
@@ -342,16 +242,4 @@ fn check_victory( board: [BoardSpace; BOARD_ROWS * BOARD_COLS ] ) -> BoardSpace 
 
     return BoardSpace::Empty;
 }
-
-/*
-fn print_board( board: [[BoardSpace; BOARD_COLS]; BOARD_ROWS] ) {
-    for row in board.iter() {
-        print!( "|" );
-        for space in row {
-            print!( " {} |", space );
-        }
-        println!( "" );
-    }
-}
-*/
 
